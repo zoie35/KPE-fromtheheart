caefe7b8cefbcc810238291aaa5298489da66583
oren <oren.levin@hotmail.com>
2025-05-25 18:01:33 +0300

    vary important message

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import faster_whisper
 model = faster_whisper.WhisperModel('ivrit-ai/whisper-large-v3-turbo-ct2')
 
 segs, _ = model.transcribe('C:/Users/USER/Desktop/לימודים/רפואה/KPE/Traumatic_024.wav', language='he')
-
+#amir - the prince of the heart - king of this repositoy
 for segment in segs:
     print(f"[{segment.start:.2f} --> {segment.end:.2f}] {segment.text}")
 
